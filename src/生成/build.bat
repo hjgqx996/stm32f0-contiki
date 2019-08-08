e0f5dea461753f54d1ba7c8c11ae9b5b431b2588
kaihua <kaihua.yang>
2019-08-08 14:07:51 +0800

    版本:0203  0001  iic delayus(5*x)  速度30kbps

--- src/生成/build.bat
+++ src/生成/build.bat
@@ -1,5 +1,8 @@
 
 del ..\生成\*.bin   /F /Q
+del ..\生成\*.hex   /F /Q
 
+stm32f0-contiki.hex
 D:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --bin --output=.\objects\v6.bin  .\objects\stm32f0-contiki.axf
-cmd.exe /C copy .\objects\v6.bin  ..\生成\LD-MZ-DRIVER-5-A-V6.bin
+cmd.exe /C copy .\objects\v6.bin               ..\生成\LD-MZ-DRIVER-5-A-V6.bin
+cmd.exe /C copy .\objects\stm32f0-contiki.hex  ..\生成\LD-MZ-DRIVER-5-A-V6.hex
